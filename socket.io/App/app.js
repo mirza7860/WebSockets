e41184e51d54e653e83a7a425c302af5d71cc2f1
Mirza Sahil Alli Baig <mirzasahilallibaig@gmail.com>
2024-01-26 17:10:29 +0530

    Real-Time-Updates-Socket.io

---
+++ socket.io/App/app.js
@@ -0,0 +1,19 @@
+const socket = io("http://localhost:8000");
+
+function sendMessage(e) {
+  e.preventDefault();
+  const input = document.querySelector("input");
+  if (input.value) {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+  input.focus();
+}
+
+document.querySelector("form").addEventListener("submit", sendMessage);
+
+socket.on("message", (data) => {
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
---
+++ socket.io/Server/index.js
@@ -0,0 +1,22 @@
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:"*"
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} is connected`);
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
+  });
+});
+
+httpServer.listen(8000, () => {
+  console.log("Server Listening at port 8000.");
+});
